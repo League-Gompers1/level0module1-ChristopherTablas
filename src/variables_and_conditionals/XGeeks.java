3efc936d59dbfb28e990d2bdf44313065e23b3a3
ChristopherTablas <christopher.tablasdelacruz@gompersstudent.org>
2017-05-01 14:56:04 -0700

    League class

--- src/variables_and_conditionals/XGeeks.java
+++ src/variables_and_conditionals/XGeeks.java
@@ -11,10 +11,70 @@ public class XGeeks {
 public static void main(String[] args) {
 
         // 1. Save the superpower for each person in a variable.
-
+		
+		String June = "Hacking";
+		
+		String Daniel = "Hacking";
+		
+		String Ebuka = "Endurance";
+				
+		String Christopher = "Reading";
+		
+		String Jesus = "Speed";
+	
+		String Jason = "Smarts";
+	
+		String Alfredo = "Gaming";
+		
+		String Joshua = "Inspiration";
+		
+		
+		
+		
         // 2. Ask the user to enter a name. Store their answer in a variable.
-
-        // 3. Show the superpower in a pop-up, depending on the name entered. 
-
+		
+		
+		
+		String name = JOptionPane.showInputDialog("Enter a name");
+		
+		
+		        // 3. Show the superpower in a pop-up, depending on the name entered. 
+		
+		if (name.equals("June")) {	
+			JOptionPane.showMessageDialog(null, "June's Superpower Is "+ June);			
+		}
+			
+		if (name.equals("Daniel")) {			
+			JOptionPane.showMessageDialog(null, "Daniel's Superpower Is "+ Daniel);			
+		}
+		
+		if (name.equals("Ebuka")) {			
+			JOptionPane.showMessageDialog(null, "Ebuka's Superpower Is "+ Ebuka);		
+		}
+		
+		if (name.equals("Christopher")) {		
+			JOptionPane.showMessageDialog(null, "Christopher's Superpower Is "+ Christopher);			
+		}
+		
+		if (name.equals("Jesus")) {			
+			JOptionPane.showMessageDialog(null, "Jesus's Superpower Is "+ Jesus);			
+		}
+		
+		if (name.equals("Jason")) {			
+			JOptionPane.showMessageDialog(null, "Jason's Superpower Is "+ Jason);			
+		}
+		
+		if (name.equals("Alfredo")) {			
+			JOptionPane.showMessageDialog(null, "Alfredo's Superpower Is "+ Alfredo);			
+		}
+		
+		if (name.equals("Joshua")) {
+			JOptionPane.showMessageDialog(null, "Joshua's Superpower Is "+ Joshua);
+		}
+		
+		
+		
+		
+		
     }
 }
